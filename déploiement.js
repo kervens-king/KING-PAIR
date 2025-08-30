d5d126c3a1bbe83fd7d0e926d2e8615cef7dc135
kervens <romeochefbratva200k@gmail.com>
2025-08-30 10:15:30 -0400

    Update déploiement.js

--- déploiement.js
+++ déploiement.js
@@ -1,20 +1,19 @@
-
 ### DEPLOYMENT
- 
-<a href='https://dashboard.heroku.com/new?template=https://github.com/PATERSON-MD/PATERSON-SESSION' target="_blank"><img alt='HEROKU DEPLOY' src='https://img.shields.io/badge/-HEROKU DEPLOY-black?style=for-the-badge&logo=heroku&logoColor=white'/>
- <br>
-<a href='https://dashboard.render.com' target="_blank">
-    <img alt='DEPLOY TO RENDER' src='https://img.shields.io/badge/-DEPLOY TO RENDER-black?style=for-the-badge&logo=render&logoColor=white'/>
-</a>
- <br>
-<a href='https://app.koyeb.com' target="_blank">
-    <img alt='DEPLOY TO KOYEB' src='https://img.shields.io/badge/-DEPLOY TO KOYEB-black?style=for-the-badge&logo=koyeb&logoColor=white'/>
-</a>
 
+<a href='https://dashboard.heroku.com/new?template=https://github.com/PATERSON-MD/PATERSON-MD' target="_blank">
+  <img alt='HEROKU DEPLOY' src='https://img.shields.io/badge/-HEROKU_DEPLOY-black?style=for-the-badge&logo=heroku&logoColor=white'/>
+</a>
+<br>
+<a href='https://render.com/deploy?repo=https://github.com/PATERSON-MD/PATERSON-MD' target="_blank">
+  <img alt='DEPLOY TO RENDER' src='https://img.shields.io/badge/-DEPLOY_TO_RENDER-black?style=for-the-badge&logo=render&logoColor=white'/>
+</a>
+<br>
+<a href='https://app.koyeb.com/deploy?type=git&repository=https://github.com/PATERSON-MD/PATERSON-MD' target="_blank">
+  <img alt='DEPLOY TO KOYEB' src='https://img.shields.io/badge/-DEPLOY_TO_KOYEB-black?style=for-the-badge&logo=koyeb&logoColor=white'/>
+</a>
 
 # `Owner`
 
- <a href="https://github.com/PATERSON-MD"><img src="https://github.com/PATERSON-MD.png" width="250" height="250" alt="Malvin Tech"/></a>
-
-
-   
+<a href="https://github.com/PATERSON-MD">
+  <img src="https://github.com/PATERSON-MD.png" width="250" height="250" alt="PATERSON-MD"/>
+</a>
